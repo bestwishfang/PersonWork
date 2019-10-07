c9d2b7edfd7b9834a9a9a9f9d48cab45319bf217
bestwishfang <bestwishfang@126.com>
2019-10-07 20:20:51 +0800

    flask项目请假功能

--- Flasknew/app/main/views.py
+++ Flasknew/app/main/views.py
@@ -17,8 +17,74 @@ def set_pwd(password):
     return ret
 
 
-def login_valid(fun):
-    @functools.wraps(fun)  # 保留原函数的名称
+class Pager:
+    """
+    flask分页通过sqlalachemy查询进行分页
+    offset 偏移，开始查询的位置
+    limit 单页条数
+    分页器需要具备的功能
+    页码
+    分页数据
+    是否第一页
+    是否最后一页
+    """
+
+    def __init__(self, data, page_size):
+        """
+
+        :param data: 要分页的数据
+        :param page_size: 每页多少条
+        """
+        self.data = data  # 总数据
+        self.page_size = page_size  # 单页数据
+        self.is_start = False
+        self.is_end = False
+        self.page_count = len(data)
+        self.next_page = 0  # 下一页
+        self.previous_page = 0  # 上一页
+        self.page_nmuber = self.page_count / page_size
+        # (data+page_size-1)//page_size
+        if self.page_nmuber == int(self.page_nmuber):
+            self.page_nmuber = int(self.page_nmuber)
+        else:
+            self.page_nmuber = int(self.page_nmuber) + 1
+
+        self.page_range = range(1, self.page_nmuber + 1)  # 页码范围
+
+    def page_data(self, page):
+        """
+        返回分页数据
+        :param page: 页码
+        page_size = 10
+        1    offect 0  limit(10)
+        2    offect 10 limit(10)
+        page_size = 10
+        1     start 0   end  10
+        2     start 10   end  20
+        3     start 20   end  30
+        """
+        self.next_page = int(page) + 1
+        self.previous_page = int(page) - 1
+        if page <= self.page_range[-1]:
+            page_start = (page - 1) * self.page_size
+            page_end = page * self.page_size
+            # data = self.data.offset(page_start).limit(self.page_size)
+            data = self.data[page_start:page_end]
+            if page == 1:
+                self.is_start = True
+            else:
+                self.is_start = False
+            if page == self.page_range[-1]:
+                self.is_end = True
+            else:
+                self.is_end = False
+        else:
+            data = ["没有数据"]
+        return data
+
+
+def login_valid(func):
+    @functools.wraps(func)  # 保留原函数的名称
     def inner(*args, **kwargs):
         username = request.cookies.get("username")
         id = request.cookies.get("id", "0")
@@ -26,7 +92,7 @@ def login_valid(fun):
         session_username = session.get("username")
         if user:  # 检测是否有对应id的用户
             if user.user_name == username and username == session_username:  # 用户名是否对应
-                return fun(*args, **kwargs)
+                return func(*args, **kwargs)
             else:
                 return redirect("/login/")
         else:
@@ -93,3 +159,38 @@ def logout():
 def index():
     name = 'fang'
     return render_template("index.html", **locals())
+
+
+@app.route("/holiday_leave/", methods=["GET", "POST"])
+# @csrf.exempt
+def holiday_leave():
+    if request.method == "POST":
+        data = request.form
+        request_user = data.get("request_user")
+        request_type = data.get("request_type")
+        start_time = data.get("start_time")
+        end_time = data.get("end_time")
+        phone = data.get("phone")
+        request_description = data.get("request_description")
+
+        leave = Leave()
+        leave.request_id = request.cookies.get("id")
+        leave.request_name = request_user
+        leave.request_type = request_type  # 假期类型
+        leave.request_start_time = start_time  # 起始时间
+        leave.request_end_time = end_time  # 结束时间
+        leave.request_description = request_description  # 请假事由
+        leave.request_phone = phone  # 联系方式
+        leave.request_status = "0"  # 假条状态
+        leave.save()
+        return redirect("/leave_list/1/")
+    return render_template("holiday_leave.html")
+
+
+@main.route("/leave_list/<int:page>/")
+@login_valid
+def leave_list(page):
+    leaves = Leave.query.all()
+    pager = Pager(leaves, 2)
+    page_data = pager.page_data(page)
+    return render_template("leave_list.html", **locals())
--- Flasknew/app/models.py
+++ Flasknew/app/models.py
@@ -21,3 +21,21 @@ class User(BaseModel):
     user_name = models.Column(models.String(32))
     password = models.Column(models.String(32))
     email = models.Column(models.String(32))
+
+
+class Leave(BaseModel):
+    """
+    请假  0
+    批准  1
+    驳回  2
+    销假  3
+    """
+    __tablename__ = "leave"
+    request_id = models.Column(models.Integer)  # 请假人id
+    request_name = models.Column(models.String(32))  # 请假人姓名
+    request_type = models.Column(models.String(32))  # 假期类型
+    request_start_time = models.Column(models.String(32))  # 起始时间
+    request_end_time = models.Column(models.String(32))  # 结束时间
+    request_description = models.Column(models.Text)  # 请假事由
+    request_phone = models.Column(models.String(32))  # 联系方式
+    request_status = models.Column(models.String(32))  # 假条状态
